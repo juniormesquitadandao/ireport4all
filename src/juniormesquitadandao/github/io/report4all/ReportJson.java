64ed5b39c275262d60967dd88e5199a191f4832b
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-11-26 22:47:10 -0300

    1.0

--- src/juniormesquitadandao/github/io/report4all/ReportJson.java
+++ src/juniormesquitadandao/github/io/report4all/ReportJson.java
@@ -25,41 +25,56 @@ public class ReportJson extends AbstractSampleApp {
         File[] jrxmls = folder.listFiles(new FileFilter() {
             @Override
             public boolean accept(File file) {
-                return file.getName().endsWith(".jrxml");
+                return file.getName().endsWith(".jrxml") && isChanged(file);
             }
         });
 
         for (File jrxml : jrxmls) {
             jasperCompileManager.compileToFile(jrxml.getAbsolutePath());
+            jrxml.setLastModified(0);
         }
-        System.err.println("Filling time : " + (System.currentTimeMillis() - start));
+        System.err.println("Compiling time : " + (System.currentTimeMillis() - start));
     }
 
     public void fill() throws JRException {
         long start = System.currentTimeMillis();
-        Map<String, Object> params = new HashMap<String, Object>();
+        Map<String, Object> params = new HashMap<>();
         params.put(JsonQueryExecuterFactory.JSON_DATE_PATTERN, "yyyy-MM-dd");
         params.put(JsonQueryExecuterFactory.JSON_NUMBER_PATTERN, "#,##0.##");
         params.put(JsonQueryExecuterFactory.JSON_LOCALE, Locale.ENGLISH);
         params.put(JRParameter.REPORT_LOCALE, Locale.US);
 
-        JasperFillManager.fillReportToFile("reports/JsonCustomersReport.jasper", params);
+        File jasper = new File("reports/JsonCustomersReport.jasper");
+        if (isChanged(jasper)) {
+            JasperFillManager.fillReportToFile(jasper.getAbsolutePath(), params);
+            jasper.setLastModified(0);
+        }
         System.err.println("Filling time : " + (System.currentTimeMillis() - start));
     }
 
-    public void pdf() throws JRException {
+    public void export() throws JRException {
         long start = System.currentTimeMillis();
-        JasperExportManager.exportReportToPdfFile("reports/JsonCustomersReport.jrprint");
-        System.err.println("PDF creation time : " + (System.currentTimeMillis() - start));
+
+        File jrprint = new File("reports/JsonCustomersReport.jrprint");
+        if (isChanged(jrprint)) {
+            JasperExportManager.exportReportToPdfFile(jrprint.getAbsolutePath());
+            jrprint.setLastModified(0);
+        }
+        System.err.println("Exporting time : " + (System.currentTimeMillis() - start));
     }
 
+    @Override
     public void test() throws JRException {
         compile();
         fill();
-        pdf();
+        export();
+    }
+
+    private boolean isChanged(File file) {
+        return file.lastModified() != 0;
     }
 
     public static void main(String[] args) throws JRException {
-        main(new ReportJson(), new String[]{"pdf"});
+        main(new ReportJson(), new String[]{"test"});
     }
 }
