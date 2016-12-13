c8cbef3501c377645efe462c0f0e191222a5e26e
juniormesquitadandao <juniormesquitadandao@gmail.com>
2016-12-12 21:59:58 -0300

    web

--- src/juniormesquitadandao/github/io/report4all/Report.java
+++ src/juniormesquitadandao/github/io/report4all/Report.java
@@ -1,16 +1,25 @@
 package juniormesquitadandao.github.io.report4all;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
-import java.util.HashMap;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
+import javax.xml.bind.DatatypeConverter;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JsonDataSource;
 import net.sf.jasperreports.engine.query.JsonQueryExecuterFactory;
 import net.sf.jasperreports.engine.util.AbstractSampleApp;
 
@@ -18,37 +27,23 @@ public class Report extends AbstractSampleApp {
 
     private final String[] args;
     private File jasper;
-    private File jrprint;
     private File pdf;
-    private File json;
-    private File error;
     private Locale outputLanguage;
+    private Map<String, Object> params;
+    private JsonDataSource dataSource;
+    private JasperPrint jasperPrint;
 
     public Report(String[] args) {
         this.args = args;
     }
 
     public void init() throws JRException {
-        try {
-            jasper = new File(args[0]);
-            json = new File(jasper.getAbsolutePath().replace(".jasper", ".json"));
-            jrprint = new File(jasper.getAbsolutePath().replace(".jasper", ".jrprint"));
-            pdf = new File(jasper.getAbsolutePath().replace(".jasper", ".pdf"));
-            error = new File(jasper.getAbsolutePath().replace(".jasper", ".pdf"));
-
-            pdf.delete();
-
-            outputLanguage = new Locale(args[1].split("-")[0], args[1].split("-")[1]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new JRException("Required Args: jasperPath, outputLanguage");
-        } catch (Exception e) {
-            throw new JRException(e.toString(), e.getCause());
-        }
+        extractValues();
     }
 
     public void fill() throws JRException {
-        Map<String, Object> params = new HashMap<>();
-        params.put("net.sf.jasperreports.json.source", json.getAbsolutePath());
+        params.put(JRParameter.REPORT_DATA_SOURCE, dataSource);
+//        params.put("net.sf.jasperreports.json.source", json.getAbsolutePath());
 //        params.put(JsonQueryExecuterFactory.JSON_DATE_PATTERN, "yyyy-MM-dd");
 //        params.put(JsonQueryExecuterFactory.JSON_NUMBER_PATTERN, "#,##0.##");
 
@@ -56,38 +51,117 @@ public class Report extends AbstractSampleApp {
 //        params.put(JsonQueryExecuterFactory.JSON_LOCALE, new Locale("pt", "BR"));
         params.put(JRParameter.REPORT_LOCALE, outputLanguage);
 //        params.put(JRParameter.REPORT_LOCALE, Locale.US);
-
-        JasperFillManager.fillReportToFile(jasper.getAbsolutePath(), params);
+        jasperPrint = JasperFillManager.fillReport(jasper.getAbsolutePath(), params);
     }
 
     public void export() throws JRException {
-        JasperExportManager.exportReportToPdfFile(jrprint.getAbsolutePath());
+        JasperExportManager.exportReportToPdfFile(jasperPrint, pdf.getAbsolutePath());
     }
 
     @Override
     public void test() throws JRException {
-        try {
-            init();
-            fill();
-            export();
-        } catch (JRException e) {
-            try {
-                Files.write(error.toPath(), e.toString().getBytes("UFT-8"), StandardOpenOption.CREATE);
-            } catch (IOException ex) {
-            }
-            throw new JRException(e);
-        }
+        init();
+        fill();
+        export();
     }
 
+//    public static void main(String[] args) throws IOException {
+//        File file = new File("reports/demo.jasper");
+//
+//        args = new String[]{file.getAbsolutePath(), "pt-BR"};
+//
+//        main(new Report(args), new String[]{"test"});
+//    }
+//    public static void main(String[] args) {
+//        main(new Report(args), new String[]{"test"});
+//    }
     public static void main(String[] args) throws IOException {
-        File file = new File("reports/demo.jasper");
+        File jasper = new File("reports/demo.jasper");
 
-        args = new String[]{file.getAbsolutePath(), "pt-BR"};
+        File json = new File("reports/demo.json");
+        String data = new String(Files.readAllBytes(json.toPath()), Charset.forName("UTF-8"));
+
+        File pdf = new File("reports/demo-" + Calendar.getInstance().getTimeInMillis() + ".pdf");
+
+        String params = "{\"key\":\"value\"}";
+
+//        String report = "{\n"
+//                + "\"jasper\":\"" + jasper.getAbsolutePath() + "\",\n"
+//                + "\"data\":" + data + ","
+//                + "\"locale\":\"pt-BR\","
+//                + "\"params\":" + params + ",\n"
+//                + "\"pdf\":\"" + pdf.getAbsolutePath() + "\"\n"
+//                + "}";
+
+        String report = "{\n"
+                + "\"jasper\":\"" + jasper.getAbsolutePath() + "\",\n"
+                + "\"data\":" + data + ","
+                + "\"locale\":\"pt-BR\","
+                + "\"params\":" + params + "\n"
+                + "}";
+
+        String base64 = DatatypeConverter.printBase64Binary(report.getBytes("UTF-8"));
+
+        args = new String[]{base64};
 
         main(new Report(args), new String[]{"test"});
     }
 
-//    public static void main(String[] args) {
-//        main(new Report(args), new String[]{"test"});
-//    }
+    private void extractValues() throws JRException {
+        try {
+            String decoded = new String(DatatypeConverter.parseBase64Binary(args[0]), "UTF-8");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> map = objectMapper.readValue(decoded, new TypeReference<Map<String, Object>>() {
+            });
+
+            System.out.println(map);
+
+            extractJasper(map);
+            extractData(map);
+            extractLocale(map);
+            extractParams(map);
+            extractPdf(map);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new JRException("Required Args: base64values");
+        } catch(JsonParseException e){
+            throw new JRException("Options invalid format json");
+        } catch (Exception e) {
+            throw new JRException(e.toString(), e.getCause());
+        }
+    }
+
+    private void extractJasper(Map<String, Object> map) {
+        jasper = new File((String) map.get("jasper"));
+    }
+
+    private void extractData(Map<String, Object> map) throws JsonProcessingException, JRException {
+        byte[] bytes = new ObjectMapper().writeValueAsBytes(map.get("data"));
+        dataSource = new JsonDataSource(new ByteArrayInputStream(bytes));
+    }
+
+    private void extractLocale(Map<String, Object> map) {
+        String localeString = (String) map.get("locale");
+        outputLanguage = new Locale(localeString.split("-")[0], localeString.split("-")[1]);
+    }
+
+    private void extractParams(Map<String, Object> map) throws IOException {
+        params = (Map<String, Object>) map.get("params");
+
+        if (params == null) {
+            params = new LinkedHashMap<>();
+        }
+    }
+
+    private void extractPdf(Map<String, Object> map) {
+        String pdfString = (String) map.get("pdf");
+
+        if (pdfString == null) {
+            pdf = new File(jasper.getAbsolutePath().replace(".jasper", ".pdf"));
+        } else {
+            pdf = new File(pdfString);
+        }
+
+        pdf.delete();
+    }
 }
